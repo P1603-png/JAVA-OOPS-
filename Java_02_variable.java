5ca730773fae6e7ec2a176be6eda65714cab7883
P1603-png <prachimote1603@gmail.com>
2025-04-02 05:29:10 -1200

    Add files via upload

---
+++ Java_02_variable.java
@@ -0,0 +1,18 @@
+public class Java_02_variable
+{
+public static void main(String[] args)
+{
+	/*primitive type
+byte age=30;
+int phone=1234567890;
+long phone2=12345678900L;
+float pi=3.14F;
+char letter='@';
+boolean isAdult=true;*/
+
+/*non-primitive type*/
+String name="Prachi";
+System.out.println(name.length());
+}
+
+}
